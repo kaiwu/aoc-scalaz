45a3ef69874ec18134f093df6db7b7710a2bd726
kaiwu <kaiwu2004@gmail.com>
2022-04-25 22:54:31 +0800

    some more tests

---
+++ src/test/scala/aoc/Test1.scala
@@ -0,0 +1,13 @@
+package aoc
+import org.junit.Assert._
+import org.junit.Test
+
+import aoc.common
+
+class Test1 {
+  @Test def plus(): Unit = {
+    assertEquals(common.plus(2, 4) , 6)
+  }
+}
+
+
