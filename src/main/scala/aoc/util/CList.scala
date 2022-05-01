b01c9a5334b99168bbc63f1a8cde988ae0552b6e
kaiwu <kaiwu2004@gmail.com>
2022-05-01 15:10:44 +0800

    clist object

--- src/main/scala/aoc/util/CList.scala
+++ src/main/scala/aoc/util/CList.scala
@@ -5,16 +5,60 @@ import scala.language.implicitConversions
 import scala.scalanative.libc._
 import scala.scalanative.unsafe._
 import scala.scalanative.unsigned._
+import scala.annotation.targetName
 import aoc.util.PPtr
+import aoc.util.Allocator
 
 type DoubleLink = CStruct2[PPtr[Byte], PPtr[Byte]]
 type CList[T]   = CStruct2[DoubleLink, T]
+
+object CList {
+  implicit def nil(implicit p: Ptr[DoubleLink]): Ptr[DoubleLink] = {
+    p.next = p
+    p.prev = p
+    p
+  }
+  def make[T: Tag](e: T): Ptr[CList[T]] = {
+    val alloc = summon[Allocator[CList[T]]]
+    val p = alloc.alloc()
+    !p.value = e
+    p.create()
+    p
+  }
+  def apply[T: Tag](e: T)(implicit p: Ptr[CList[T]]): Ptr[CList[T]] = {
+    !p.value = e
+    p.create()
+    p
+  }
+  def apply[T: Tag](ls: T*): Ptr[CList[T]] = ???
+  @targetName("::")
+  def ::[T: Tag](l1: Ptr[CList[T]], l2: Ptr[CList[T]]): Ptr[CList[T]] = {
+    l1.add_tail(l2)
+  }
+}
 
 final case class DoubleLinkOps(d: Ptr[DoubleLink]) {
   def prev: Ptr[DoubleLink]            = !d.at1.asInstanceOf[PPtr[DoubleLink]]
   def next: Ptr[DoubleLink]            = !d.at2.asInstanceOf[PPtr[DoubleLink]]
   def prev_=(p: Ptr[DoubleLink]): Unit = !d.at1.asInstanceOf[PPtr[DoubleLink]] = p
   def next_=(p: Ptr[DoubleLink]): Unit = !d.at2.asInstanceOf[PPtr[DoubleLink]] = p
+
+  // d o d->next
+  def con_head(o: Ptr[DoubleLink]) : Ptr[DoubleLink] = {
+    d.next.prev = o
+    o.next = d.next
+    d.next = o
+    o.prev = d
+    d
+  }
+  // d->prev o d
+  def con_tail(o: Ptr[DoubleLink]) : Ptr[DoubleLink] = {
+    d.prev.next = o
+    o.prev = d.prev
+    o.next = d
+    d.prev = o
+    d
+  }
 }
 given Conversion[Ptr[DoubleLink], DoubleLinkOps]       = DoubleLinkOps(_)
 given [T: Tag]: Conversion[Ptr[CList[T]], CListOps[T]] = new CListOps[T](_)
@@ -31,17 +75,20 @@ final case class CListOps[T: Tag](h: Ptr[CList[T]]) {
     node.next = next
     next.prev = node
   }
-  def add_head(node: Ptr[CList[T]]): Unit = {
+  def add_head(node: Ptr[CList[T]]): Ptr[CList[T]] = {
     if (!node.valid) node.create()
     add(node.link, h.link, h.link.next)
+    h
   }
-  def add_tail(node: Ptr[CList[T]]): Unit = {
+  def add_tail(node: Ptr[CList[T]]): Ptr[CList[T]] = {
     if (!node.valid) node.create()
     add(node.link, h.link.prev, h.link)
+    h
   }
-  def del(node: Ptr[CList[T]]): Unit = {
+  def del(node: Ptr[CList[T]]): Ptr[CList[T]] = {
     node.link.prev.next = node.link.next
     node.link.next.prev = node.link.prev
+    h
   }
 
   def foreach[U](f: T => U): Unit = {
--- src/test/scala/aoc/util/CListTest.scala
+++ src/test/scala/aoc/util/CListTest.scala
@@ -6,19 +6,10 @@ import scala.language.implicitConversions
 import scala.scalanative.unsafe.*
 import scala.scalanative.unsigned.*
 import scala.scalanative.libc.*
+import aoc.util.CList.::
 
 class CListTest {
   @Test def create(): Unit = Zone { implicit z =>
-    val head = alloc[CList[CInt]]()
-    head.create()
-    assertTrue(head.valid)
-    assertTrue(head.empty)
-    for (i <- 1 to 10) {
-      val a = alloc[CList[CInt]]()
-      !a.value = i
-      head.add_tail(a)
-    }
-    assertFalse(head.empty)
-    assertEquals(head.size, 10.toULong)
+    implicit def allocator[T]: Ptr[CList[T]] = alloc[CList[T]]()
   }
 }
